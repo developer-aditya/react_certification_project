16d6373fbfa7a020a012a8022ed0e5e804b96c44
Aditya Singh <adityathedev@gmail.com>
2020-07-17 21:54:49 +0530

    Creating Menu as React Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+
+import Menu from './components/MenuComponents.js'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar dark color="primary">
+    <div>
+      <Navbar dark color="dark">
         <div className="container">
           <NavbarBrand href="#">Hello Bois</NavbarBrand>
         </div>
       </Navbar>
+
+      <Menu />
     </div>
   );
 }
